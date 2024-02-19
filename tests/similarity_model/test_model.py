76b988d438be5464c0819982d012abbf6e5106d2
Lamjar <lamjar.oumayma1@gmail.com>
2024-02-19 16:53:45 +0100

    fixtest

--- tests/similarity_model/test_model.py
+++ tests/similarity_model/test_model.py
@@ -2,18 +2,16 @@ import pytest
 from src.similarity.model import Similarity
 
 
-class Test():
-
-    
-    def predict_test():
-        model = Similarity.compute()
+class Test:    
+    def test_predict(self):
+        model = Similarity()
 
         sentence1 = "The cat sits outside" 
         sentence2 = "The dog plays in the garden"
 
-        predict = model(sentence1 , sentence2)
+        predict = model.compute(sentence1 , sentence2)
 
-        return isinstance(predict , float )
+        assert isinstance(predict , float) == True
 
 
 
