1dd175bc3a8b824790c55db110b4c59cfb51b42f
Lamjar797 <160475344+Lamjar797@users.noreply.github.com>
2024-02-19 11:38:37 +0100

    add model similarity (#1)

---
+++ src/similarity/model.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer, util
+
+
+class Similarity:
+    def __init__(self)-> None:
+        self.model= SentenceTransformer("all-MiniLM-L6-v2")
+
+    def compute(self , sentence1 , sentence2)-> float:
+
+        embeddings1 = self.model.encode(sentence1, convert_to_tensor=True)
+        embeddings2 = self.model.encode(sentence2, convert_to_tensor=True)
+
+        # Compute cosine-similarities
+        cosine_scores = util.cos_sim(embeddings1, embeddings2)
+
+        return cosine_scores
